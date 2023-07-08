ab6fdd4d58dca18525c1138797afbc8dd8000af2
Eliana Ferreira <ebferreiran@gmail.com>
2023-07-08 12:29:28 -0400

    displaying the taken image in the Detail Screen

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -11,8 +11,15 @@ class PlaceDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(place.title)),
-      body: Center(
-        child: Text(place.title),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          )
+        ],
       ),
     );
   }
